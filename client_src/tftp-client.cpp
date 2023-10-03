43685875e48019829b982f0bc8df93984bc52943
Denis Novosád <dnovosad@mediatel.cz>
2023-10-03 14:52:28 +0200

    sending works perfect recieving fixing

--- client_src/tftp-client.cpp
+++ client_src/tftp-client.cpp
@@ -156,27 +156,18 @@ void handleError(int sock, const std::string &hostname, int srcPort, int dstPort
 }
 
 // Function to send a file to the server or upload from stdin
-void transfer_file(int sock, const std::string &hostname, int port, const std::string &localFilePath, const std::string &remoteFilePath, const std::string &mode, const std::string &options)
+void SendFile(int sock, const std::string &hostname, int port, const std::string &localFilePath, const std::string &remoteFilePath, const std::string &mode, const std::string &options)
 {
     std::istream *inputStream;
     std::ifstream file;
 
-    if (localFilePath.empty())
-    {
+    std::string userInput;
+    std::cout << "Zadejte řádek textu: ";
+    std::getline(std::cin, userInput);
 
-        std::string userInput;
-        std::cout << "Zadejte řádek textu: ";
-        std::getline(std::cin, userInput);
+    std::cout << "Zadali jste: " << userInput << std::endl;
 
-        std::cout << "Zadali jste: " << userInput << std::endl;
-
-        file.open(userInput, std::ios::binary);
-    }
-    else
-    {
-        // Try to open the file for reading
-        file.open(localFilePath, std::ios::binary);
-    }
+    file.open(userInput, std::ios::binary);
 
     if (!file)
     {
@@ -257,6 +248,164 @@ void transfer_file(int sock, const std::string &hostname, int port, const std::s
     close(sock);
 }
 
+// Function to send RRQ (Read Request) packet
+void sendReadRequest(int sock, const std::string &hostname, int port, const std::string &remoteFilePath, const std::string &mode, const std::string &options)
+{
+    // Create an RRQ packet (opcode 1)
+    std::vector<uint8_t> requestBuffer;
+    requestBuffer.push_back(0); // High byte of opcode (0 for RRQ)
+    requestBuffer.push_back(1); // Low byte of opcode (1 for RRQ)
+    requestBuffer.insert(requestBuffer.end(), remoteFilePath.begin(), remoteFilePath.end());
+    requestBuffer.push_back(0); // Null-terminate the filepath
+    requestBuffer.insert(requestBuffer.end(), mode.begin(), mode.end());
+    requestBuffer.push_back(0); // Null-terminate the mode
+
+    // Append optional parameters if provided
+    if (!options.empty())
+    {
+        requestBuffer.insert(requestBuffer.end(), options.begin(), options.end());
+        requestBuffer.push_back(0); // Null-terminate the options
+    }
+
+    // Create sockaddr_in structure for the remote server
+    sockaddr_in serverAddr;
+    std::memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+    inet_pton(AF_INET, hostname.c_str(), &(serverAddr.sin_addr));
+
+    // Send the RRQ packet
+    ssize_t sentBytes = sendto(sock, requestBuffer.data(), requestBuffer.size(), 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+    if (sentBytes == -1)
+    {
+        std::cerr << "Error: Failed to send RRQ packet." << std::endl;
+    }
+
+    std::cerr << "Read Request " << hostname << ":" << port << " \"" << remoteFilePath << "\" " << mode;
+    if (!options.empty())
+    {
+        std::cerr << " with options: " << options;
+    }
+    std::cerr << std::endl;
+}
+
+// Function to receive DATA packet
+bool receiveData(int sock, uint16_t &receivedBlockID, std::string &data)
+{
+    // Create a buffer to receive the DATA packet
+    std::vector<uint8_t> dataBuffer(516); // Max size of a DATA packet
+
+    // Create sockaddr_in structure to store the sender's address
+    sockaddr_in senderAddr;
+    socklen_t senderAddrLen = sizeof(senderAddr);
+
+    // Receive the DATA packet and capture the sender's address
+    ssize_t receivedBytes = recvfrom(sock, dataBuffer.data(), dataBuffer.size(), 0, (struct sockaddr *)&senderAddr, &senderAddrLen);
+    if (receivedBytes == -1)
+    {
+        std::cerr << "Error: Failed to receive DATA." << std::endl;
+        return false;
+    }
+
+    // Check if the received packet is a DATA packet
+    if (receivedBytes < 4 || dataBuffer[0] != 0 || dataBuffer[1] != 3)
+    {
+        std::cerr << "Error: Received packet is not a DATA packet." << std::endl;
+        return false;
+    }
+
+    // Parse the received block ID from the DATA packet
+    receivedBlockID = (dataBuffer[2] << 8) | dataBuffer[3];
+
+    // Extract the data from the packet (skip the first 4 bytes which are the header)
+    data.assign(dataBuffer.begin() + 4, dataBuffer.begin() + receivedBytes);
+
+    // Create sockaddr_in structure for the remote server
+    sockaddr_in serverAddr;
+    std::memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = senderAddr.sin_port;
+    serverAddr.sin_addr = senderAddr.sin_addr;
+
+    // Send an ACK packet back to the server
+    std::vector<uint8_t> ackBuffer(4);
+    ackBuffer[0] = 0;                             // High byte of opcode (0 for ACK)
+    ackBuffer[1] = 4;                             // Low byte of opcode (4 for ACK)
+    ackBuffer[2] = (receivedBlockID >> 8) & 0xFF; // High byte of block ID
+    ackBuffer[3] = receivedBlockID & 0xFF;        // Low byte of block ID
+
+    ssize_t sentBytes = sendto(sock, ackBuffer.data(), ackBuffer.size(), 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+    if (sentBytes == -1)
+    {
+        std::cerr << "Error: Failed to send ACK." << std::endl;
+        return false;
+    }
+
+    std::cerr << "Received DATA with block ID: " << receivedBlockID << " from server port: " << ntohs(serverAddr.sin_port) << std::endl;
+
+    return true;
+}
+
+// Modify the transfer_file function to receive a file from the server
+void receive_file(int sock, const std::string &hostname, int port, const std::string &localFilePath, const std::string &remoteFilePath, const std::string &mode, const std::string &options)
+{
+    std::ofstream outputFile(localFilePath, std::ios::binary); // Open a local file to write the received data
+
+    if (!outputFile)
+    {
+        std::cerr << "Error: Failed to open file for writing." << std::endl;
+        close(sock); // Close the socket on error
+        return;
+    }
+
+    uint16_t blockID = 0; // Initialize block ID to 0
+
+    // Send an RRQ packet to request the file from the server
+    sendReadRequest(sock, hostname, port, remoteFilePath, mode, options);
+
+    bool transferComplete = false;
+
+    while (!transferComplete)
+    {
+        uint16_t receivedBlockID;
+        std::string data;
+
+        // Receive a DATA packet and store the data in 'data'
+        if (!receiveData(sock, receivedBlockID, data))
+        {
+            std::cerr << "Error: Failed to receive DATA." << std::endl;
+            handleError(sock, hostname, port, 0, 0, "Failed to receive DATA");
+            break;
+        }
+
+        // Check if the received block ID is the expected one
+        if (receivedBlockID != blockID + 1)
+        {
+            std::cerr << "Error: Received out-of-order block ID." << std::endl;
+            handleError(sock, hostname, port, 0, 0, "Received out-of-order block ID");
+            break;
+        }
+
+        // Write the received data to the output file
+        outputFile.write(data.c_str(), data.size());
+
+        // Increment the block ID for the next ACK
+        blockID = receivedBlockID;
+
+        // If the received data block is less than the maximum size, it indicates the end of the transfer
+        if (data.size() < 512)
+        {
+            transferComplete = true;
+        }
+    }
+
+    // Close the output file
+    outputFile.close();
+
+    // Close the socket
+    close(sock);
+}
+
 int main(int argc, char *argv[])
 {
     std::string hostname;
@@ -317,8 +466,23 @@ int main(int argc, char *argv[])
     serverAddr.sin_port = htons(port);
     inet_pton(AF_INET, hostname.c_str(), &(serverAddr.sin_addr));
 
+    if (localFilePath.empty())
+    {
+        SendFile(sock, hostname, port, localFilePath, remoteFilePath, mode, options);
+    }
+    else if (!localFilePath.empty() && !remoteFilePath.empty())
+    {
+        // Receive a file from the server
+        receive_file(sock, hostname, port, localFilePath, remoteFilePath, mode, options);
+    }
+    else
+    {
+        std::cerr << "Error: You must specify either -f or -t option to send or receive a file, respectively." << std::endl;
+        close(sock);
+        return 1;
+    }
+
     // Send the file to the server or upload from stdin
-    transfer_file(sock, hostname, port, localFilePath, remoteFilePath, mode, options);
 
     return 0;
 }
